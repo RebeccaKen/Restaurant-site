b81d8720722a5e9f68f1cd32c79598ae43f081fb
RebeccaKen <kennedyrebecca694@gmail.com>
2023-03-22 18:35:44 +0000

    Add databases to setting.py file

--- django_restaurant/settings.py
+++ django_restaurant/settings.py
@@ -77,11 +77,16 @@ WSGI_APPLICATION = 'django_restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
