b90105e888446a94cb48bdf1f8186967ffd42188
RebeccaKen <kennedyrebecca694@gmail.com>
2023-03-25 20:56:30 +0000

    Add new models to database

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
+
+
 
 # model for Menu
+
 
 class Menu(models.Model):
     name = models.CharField(max_length=200)
@@ -14,7 +18,9 @@ class Menu(models.Model):
     def get_absolute_url(self):
         return reverse('menu_detail', args=[str(self.id)])
 
+
 # model for MenuItems
+
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -22,7 +28,7 @@ class MenuItem(models.Model):
     description = models.TextField(blank=True, max_length=500)
     allergens = models.TextField(blank=True, max_length=500)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    menu = models.ForeignKey(Menu, related_name='items', on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, related_name='menu', on_delete=models.CASCADE)
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
 
@@ -32,7 +38,56 @@ class MenuItem(models.Model):
     def get_absolute_url(self):
         return reverse('menu_detail', args=[str(self.menu.id)])
 
-    
+
+# model for reservations
 
 
+class Reservation(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    time = models.TimeField()
+    number_of_guests = models.IntegerField()
+    notes = models.CharField(max_length=200, blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"Reservation for {self.party_size} people on {self.date} at {self.time}"
+
+
+# model for orders
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now=True)
+    order_total = models.DecimalField(max_digits=8, decimal_places=2)
+    reservation = models.ForeignKey(Reservation, on_delete=models.SET_NULL, null=True, blank=True)
+    items = models.ManyToManyField(MenuItem)
+
+    def __str__(self):
+        return f"Order #{self.id} for {self.customer.name}"
+
+    class Meta:
+        ordering = ['-order_date']
+
+    def total_cost(self):
+        return sum(item.price for item in self.items.all())
+
+
+# models for Customer information
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200, blank=True)
+
+
+    def __str__(self):
+        return self.name
 
