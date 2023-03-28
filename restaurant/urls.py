32f7524e1ed4f2dfc00b53f76adcc9579100a952
RebeccaKen <kennedyrebecca694@gmail.com>
2023-03-28 01:13:50 +0000

    Add lists for reservation create/update/delete

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomePageView, MenuListView, TemplateView
+from .views import HomePageView, MenuListView, TemplateView, ReservationUpdateView, ReservationDeleteView
+from .views import ReservationCreateView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('menu/', MenuListView.as_view(), name='menu_list'),
+    path('menu/', MenuListView.as_view(), name='menu'),
+    path('reservation/create', ReservationCreateView.as_view(), name='reservation'),
+    path('reservation/<int:pk>/update/', ReservationUpdateView.as_view(), name='reservation_update'),
+    path('reservation/<int:pk>/delete/', ReservationDeleteView.as_view(), name='reservation_delete')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -4,14 +4,11 @@ from .models import Menu, MenuItem, Reservation, Order, Customer
 from django.utils import timezone
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 
-# Views for restaurant website
-def get_restaurant_list(request):
-    return render(request, 'restaurant/restaurant_list.html')
-
+#Views for restaurant website
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -23,14 +20,31 @@ class MenuListView(ListView):
     context_object_name = 'menus'
 
 
-class MenuItemCreateView(LoginRequiredMixin, CreateView):
-    model = MenuItem
-    template_name = 'menu_item_form.html'
-    fields = ['name', 'slug', 'description', 'allergens', 'price', 'menu']
+class ReservationCreateView(LoginRequiredMixin, CreateView):
+    model = Reservation
+    template_name = 'reservation_form.html'
+    fields = ['name', 'email', 'phone', 'date', 'number_of_guests', 'notes',]
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+class ReservationUpdateView(LoginRequiredMixin, UpdateView):
+    model = Reservation
+    template_name = 'reservation_form.html'
+    fields = ['name', 'email', 'phone', 'date', 'number_of_guests', 'notes']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+class ReservationDeleteView(LoginRequiredMixin, DeleteView):
+    model = Reservation
+    template_name = 'reservation_confirm_delete.html'
+    success_url = reverse_lazy('reservation_list')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
 
 
